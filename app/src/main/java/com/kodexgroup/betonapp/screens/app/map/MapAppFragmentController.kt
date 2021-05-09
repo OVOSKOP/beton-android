5874dbcfefa4fc7ab9669f30f99f288f0d8f7bb6
OVOSKOP <parodia1234321@gmail.com>
2021-05-09 23:12:02 +0300

    add map

---
+++ app/src/main/java/com/kodexgroup/betonapp/screens/app/map/MapAppFragmentController.kt
@@ -0,0 +1,12 @@
+package com.kodexgroup.betonapp.screens.app.map
+
+import android.content.Context
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+
+class MapAppFragmentController(fragment: Fragment, private val context: Context, view: View) {
+
+    var map: GoogleMap? = null
+
+}
